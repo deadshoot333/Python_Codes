55c184c4e5f981b9344c71005426172ed7c21806
deadshoot333 <arqam343almas@gmail.com>
2024-03-15 23:39:55 +0600

    GUI updated

--- To_Do_Project/GUI.py
+++ To_Do_Project/GUI.py
@@ -1,29 +1,33 @@
 import PySimpleGUI as gui
 from ToDoClass import To_Do
+import time
 
+gui.theme("DarkPurple4")
 todo = To_Do()
 
-label = gui.Text("Enter to do: ")
+clock=gui.Text('clk',key='clock')
+label = gui.Text("Enter to do ")
 input_box = gui.InputText(tooltip="Enter a to do", key="todo")
-add_button = gui.Button("Add")
+add_button = gui.Button("Add",size=20)
 
 list_box = gui.Listbox(
     values=todo.show(), key="show_todos", enable_events=True, size=[45, 10]
 )
 
-edit_button = gui.Button("Edit")
-complete_button = gui.Button("Complete")
+edit_button = gui.Button("Edit",size=15)
+complete_button = gui.Button("Complete",size=15)
 exit_button = gui.Button("Exit")
 window = gui.Window(
     "My To-Do app",
-    layout=[[label], 
+    layout=[[label],
             [input_box, add_button], 
-            [list_box, edit_button, complete_button],
-            [exit_button]],
+            [list_box, edit_button,complete_button,exit_button], 
+            [clock]],
     font=("Helvectica", 12),
 )  # layout take 2D array
 while True:
-    event, kvalue = window.read()
+    event, kvalue = window.read(timeout=10)
+    window['clock'].update(value=time.strftime("%b %d,%Y %H:%M:%S"))
     print(f"1 {event}")
     print(f"2 {kvalue}")
     match event:
@@ -33,21 +37,27 @@ while True:
             todos = todo.show()
             window["show_todos"].update(values=todos)
         case "Edit":
-            todo_to_edit = kvalue["show_todos"][0]
-            new_todo = kvalue["todo"]
+            try:
+                todo_to_edit = kvalue["show_todos"][0]
+                new_todo = kvalue["todo"]
 
-            todos = todo.show()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            todo.write_todo(todos)
-            window["show_todos"].update(values=todos)
-            print(index)
+                todos = todo.show()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                todo.write_todo(todos)
+                window["show_todos"].update(values=todos)
+                print(index)
+            except IndexError:
+                gui.popup("Please select an Item first",font=("Helvectica",12))
         case "Complete":
-            todo_to_complete = kvalue["show_todos"][0]
-            todos = todo.show()
-            todos.remove(todo_to_complete)
-            todo.write_todo(todos)
-            window["show_todos"].update(values=todos)
+            try:
+                todo_to_complete = kvalue["show_todos"][0]
+                todos = todo.show()
+                todos.remove(todo_to_complete)
+                todo.write_todo(todos)
+                window["show_todos"].update(values=todos)
+            except IndexError:
+                gui.popup("Please select an Item First",font=("Helvectica",12))
         case "Exit":
             break
         case gui.WIN_CLOSED:
