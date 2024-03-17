95389ac5d8f4e9a0678e40dd1dbab249c5e5f5a2
deadshoot333 <arqam343almas@gmail.com>
2024-03-17 15:27:28 +0600

    PDF generator edited

--- PDF_Generator/main.py
+++ PDF_Generator/main.py
@@ -11,18 +11,20 @@ for index, row in df.iterrows():
     pdf.set_text_color(255, 150, 55)
     pdf.set_font(family="Times", style="B", size=20)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
-    pdf.line(10, 21, 200, 21)  ## x1,y1,x2,y2 and unit is in mm
+    for i in range(20, 298, 10):
+        pdf.line(10, i, 200, i)  ## x1,y1,x2,y2 and unit is in mm
+
     # pdf.cell(
     #     w=0, h=12, txt="HI I am first cell", align="L", ln=1, border=1
     # )  ## if w=0 then border will be in the whole width of pdf
     # pdf.set_font(family="Times", style="I", size=20)
     for i in range(row["Pages"] - 1):
         pdf.add_page()
+        for j in range(20, 290, 10):
+            pdf.line(10, j, 200, j)
         ##set footer
-        pdf.ln(265)
+        pdf.ln(270)
         pdf.set_font(family="Times", style="B", size=8)
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=5, txt=row["Topic"], align="R")
-    pdf.line(10, 280, 200, 280)
-
 pdf.output("PDF_Generator/MyPDF.pdf")
