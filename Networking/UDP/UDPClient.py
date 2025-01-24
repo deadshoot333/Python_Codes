b86ef981ca3d6a00c5f454aecb8f54eed36d7467
deadshoot333 <arqam343almas@gmail.com>
2025-01-24 21:35:17 +0600

    UDP added in networking

---
+++ Networking/UDP/UDPClient.py
@@ -0,0 +1,26 @@
+from socket import *
+
+##server initialization
+serverName 
+serverPort 
+
+##socket creation
+##AF_INET indicates that underlying network is using IPV4
+##SOCK_DGRAM means it is a UDP socket
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+
+##enter your message
+message = input("Input lowercase sentence:")
+
+##message.encode()-> convert string to bytes
+##(serverName,serverPort)-> tuple about the server
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+##2048 is buffer size
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+##serverAddress is a tuple which is indexed by [0],[1]
+print(f"Message modified from Server Name:{serverAddress[0]}\nServer Port:{serverAddress[1]}")
+
+##socket is closed
+clientSocket.close()
---
+++ Networking/UDP/UDPServer.py
@@ -0,0 +1,17 @@
+##server must run before client in a sperate terminal
+from socket import *
+
+serverPort
+serverSocket = socket(AF_INET, SOCK_DGRAM)
+
+##assigns port to socket
+serverSocket.bind(('', serverPort))
+print("The server is ready to receive")
+
+while True:
+    message, clientAddress = serverSocket.recvfrom(2048)
+    ##convert bytes to string using decode and CAPITALIZE it using upper()
+    modifiedMessage = message.decode().upper()
+    print(f'Message receive from Client Address:{clientAddress[0]}\nClient Port:{clientAddress[1]}')
+    print(f'Message changed to {modifiedMessage}')
+    serverSocket.sendto(modifiedMessage.encode(),clientAddress)
